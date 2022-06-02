25b5a18634b7c4ec82aba2f9d448850879767e90
mhmd-moradi <mohamadmuradi@Mohamads-MacBook-Air.local>
2022-06-02 23:14:00 +0300

    Added generate_id function

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,13 @@
-window.onload = function () {
-    
-}
+$(document).ready(function(){
+    var todos = {};
+
+    function generate_id(){
+        let id = Math.floor(Math.random() * 9999);
+        while((id in todos)){
+            id = Math.floor(Math.random() * 9999);
+        }
+        return id;
+    }
+
+    console.log(generate_id());
+});
